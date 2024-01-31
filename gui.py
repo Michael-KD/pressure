580586fdbf16169561f087073b251c601a1b9052
Michael Krumland-Dunning <michael@dunning.com>
2024-01-31 12:56:37 -0800

    gui

--- gui.py
+++ gui.py
@@ -4,21 +4,19 @@ from tkinter import filedialog, messagebox
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.ticker import FuncFormatter
+
 import pandas as pd
 import threading
 import time
 
-import socket
-from time import sleep
-from struct import pack
+import subprocess
+import paramiko
 
 class Gui(tk.Tk):
     def __init__(self):
 
-        self.time_refresh = 2
-
-        with open('udp_data.csv', 'w') as f:
-            f.write('Time,Pressure,Temperature\n')
+        self.time_refresh = 10 # time in seconds between each update of the graph
 
         super().__init__()
 
@@ -40,7 +38,7 @@ class Gui(tk.Tk):
             'Pressure': self.fig.add_subplot(211),  # 2 rows, 1 column, first plot
             'Temperature': self.fig.add_subplot(212)  # 2 rows, 1 column, second plot
         }
-        self.fig.subplots_adjust(left=0.04, right=0.96, bottom=0.07, top=0.95, wspace=0.1, hspace=0.25)
+        self.fig.subplots_adjust(left=0.07, right=0.99, bottom=0.07, top=0.95, wspace=0.1, hspace=0.25)
 
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.plot_frame)
         canvas_widget = self.canvas.get_tk_widget()
@@ -51,8 +49,8 @@ class Gui(tk.Tk):
         # Create the buttons
         self.control_frame = ttk.Frame(self)
         self.control_frame.grid(row=1, column=0, padx=10, pady=10, sticky="ew")
-        ttk.Button(self.control_frame, text='Start Test', command=self.start_test).grid(row=0, column=1, sticky="e")
-        ttk.Button(self.control_frame, text='End Test', command=self.end_test).grid(row=0, column=2, sticky="e")
+        ttk.Button(self.control_frame, text='Graph', command=self.graph_data).grid(row=0, column=1, sticky="e")
+        ttk.Button(self.control_frame, text='SSH', command=self.ssh).grid(row=0, column=2, sticky="e")
 
         # do i need all these?
         self.grid_columnconfigure(0, weight=1)
@@ -60,34 +58,7 @@ class Gui(tk.Tk):
 
 
     def start_gui(self):
-        """Starts the gui and the thread"""
-        self.running = True
-        self.thread = threading.Thread(target=self.update_data)
-        self.thread.daemon = True
         self.mainloop()
-
-    def update_data(self):
-        """Updates the dataframe, df, from udp_data.csv"""
-        last_run_time = time.time()
-        while self.running:
-            if time.time() - last_run_time >= self.time_refresh:
-                try:
-                    # Receive data over UDP
-                    data, address = self.sock.recvfrom(4096)
-                    data = data.decode()
-                    print(data)
-
-                    # Write the data to the CSV file
-                    self.csv_file.write(data)
-
-                    df = pd.read_csv("udp_data.csv")
-                    
-                    # Update the graph with the new data
-                    self.safe_after(0, self.update_graph, df)            
-
-                    last_run_time = time.time()
-                except pd.errors.EmptyDataError:
-                    print("udp_data.csv is empty. Waiting for data...")
                     
     def update_graph(self, df):
         """Updates the graph with the new data"""
@@ -96,51 +67,95 @@ class Gui(tk.Tk):
 
         for column in ['Pressure', 'Temperature']:
             ax = self.axes[column]
-            ax.plot(df["Time"], df[column], marker='o', label=column)
+            ax.plot(df["Time"], df[column], label=column)
             ax.set_title(f"{column}")
             ax.set_xlabel("Time")
             ax.set_ylabel(column)
             ax.grid(True)
+            if column == 'Pressure':
+                formatter = FuncFormatter(lambda y, _: '{:.16g}'.format(y))
+                ax.yaxis.set_major_formatter(formatter)
+
 
         self.canvas.draw()
 
     def on_closing(self):
-        """Called when closing the gui, destroys the gui and closes the thread"""
-        # i think order is correct/safest but im not entirely sure
+        """Called when closing the gui, destroys the gui"""
         self.running = False
-        self.destroy()
-        time.sleep(self.time_refresh)
-        if self.thread.is_alive():
-            self.thread.join()
         self.quit()
-
+        self.destroy()
+    
     def safe_after(self, delay, func, *args, **kwargs):
         """Executes a function after a delay, but only if the program is still running."""
         if self.running:
             self.after(delay, func, *args, **kwargs)
 
+
     def start_test(self):
         """Starts the test"""
-        if self.thread is not None:
-            # Create a UDP socket
-            self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        if self.thread is None or not self.thread.is_alive():
+            print("Starting test...")
 
-            # Bind the socket to the port
-            server_address = ('0.0.0.0', 12345)
-            self.sock.bind(server_address)
+            # Create an SSH client
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-            # Open the CSV file
-            self.csv_file = open('udp_data.csv', 'w')
+            # Connect to the server
+            print("Connecting to the server...")
+            ssh.connect('169.231.49.78', username='lubin', password='spaceball') ### change ip
+            print("Connected to the server.")
 
+            # Use SCP to download the file
+            print("Downloading the file...")
+            scp = ssh.open_sftp()
+            scp.get('/home/lubin/Code/pressuresensor/data.csv', 'C:/Users/micha/Documents/Code/LUBIN_LAB/pressuresensor/download_data.csv')
+            scp.close()
+            print("File downloaded.")
+
+            # Start the thread to update the data
+            print("Starting the data update thread...")
             self.running = True
+            self.thread = threading.Thread(target=self.update_data)
             self.thread.start()
+            print("Data update thread started.")
+
+    def graph_data(self):
+        """Downloads the file and graphs it"""
+        print("Downloading the file...")
+
+        # Create an SSH client
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        # Connect to the server
+        print("Connecting to the server...")
+        ssh.connect('169.231.49.78', username='lubin', password='spaceball') ### change ip
+        print("Connected to the server.")
+
+        # Use SCP to download the file
+        scp = ssh.open_sftp()
+        scp.get('/home/lubin/Code/pressuresensor/data.csv', 'C:/Users/micha/Documents/Code/LUBIN_LAB/pressuresensor/download_data.csv')
+        scp.close()
+        print("File downloaded.")
+
+        # Update the graph with the new data
+        try:
+            df = pd.read_csv("download_data.csv")
+            self.update_graph(df)
+        except pd.errors.EmptyDataError:
+            print("download_data.csv is empty. Waiting for data...")
+
+    def ssh(self):
+        command = 'ssh -i C:\\Users\\micha\\.ssh\\id_rsa lubin@169.231.49.78'
+        subprocess.Popen(['start', 'cmd', '/k', command], shell=True)
+
 
 
     def end_test(self):
         """Ends the test"""
         if self.running == True:
             self.running = False
-            if self.thread.is_alive():
+            if self.thread is not None and self.thread.is_alive():
                 self.thread.join()
             self.thread = None
             self.current_time = 0
